2f5c6d82965002044f0ae0063039b1bd8ff985a0
frontdesk-beep <frontdesk@careinsurance.ca>
2026-08-25 22:17:54 -0400

    last commit

--- Task-Manager-Care/Program.cs
+++ Task-Manager-Care/Program.cs
@@ -103,16 +103,16 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var context = scope.ServiceProvider
-            .GetRequiredService<AppDbContext>();
+//if (app.Environment.IsDevelopment())
+//{
+//    using (var scope = app.Services.CreateScope())
+//    {
+//        var context = scope.ServiceProvider
+//            .GetRequiredService<AppDbContext>();
 
-        await DbSeeder.SeedAsync(context);
-    }
-}
+//        await DbSeeder.SeedAsync(context);
+//    }
+//}
 
 // Your existing middleware
 app.UseHttpsRedirection();
